1f53d747cd7f3be783a0518640d658a1febb73d7
Jeremy Nancel <jeremy@nancel.net>
2020-12-01 15:56:07 +0100

    AZ-373 Update variable name

--- r-logs.tf
+++ r-logs.tf
@@ -4,9 +4,9 @@ module "logging" {
 
   name = local.diag_settings_name
 
-  logs_destination_ids = var.logs_destination_ids
-  resource_id          = azurerm_api_management.apim.id
-  retention_days       = var.logs_storage_retention
+  logs_destinations_ids = var.logs_destinations_ids
+  resource_id           = azurerm_api_management.apim.id
+  retention_days        = var.logs_storage_retention
 
   log_categories    = var.log_categories
   metric_categories = var.metric_categories
