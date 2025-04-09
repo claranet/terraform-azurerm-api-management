1e80a383f494aa6ff1b6283e4dbd1a33bc686e6c
Jérôme Respaut <jerome.respaut@claranet.com>
2025-04-09 15:26:26 +0200

    feat(AZ-1547): adds backend configuration support for API Management
    Introduces a new variable to define backend configurations for API Management.
    Implements the `azurerm_api_management_backend` resource to manage backends dynamically.
    Updates documentation to include the new backend resource and its configuration details.

---
+++ r-backend.tf
@@ -0,0 +1,44 @@
+resource "azurerm_api_management_backend" "main" {
+  for_each = { for backend in var.backends : backend.name => backend }
+
+  resource_group_name = var.resource_group_name
+  api_management_name = azurerm_api_management.main.name
+
+  name        = each.value.name
+  protocol    = each.value.protocol
+  url         = each.value.url
+  description = each.value.description
+
+  resource_id = each.value.resource_id
+  title       = each.value.title
+
+  dynamic "credentials" {
+    for_each = each.value.credentials != null ? [each.value.credentials] : []
+    content {
+      authorization {
+        parameter = credentials.value.authorization.parameter
+        scheme    = credentials.value.authorization.scheme
+      }
+      certificate = credentials.value.certificate
+      header      = credentials.value.header
+      query       = credentials.value.query
+    }
+  }
+
+  dynamic "proxy" {
+    for_each = each.value.proxy != null ? [each.value.proxy] : []
+    content {
+      url      = proxy.value.url
+      username = proxy.value.username
+      password = proxy.value.password
+    }
+  }
+
+  dynamic "tls" {
+    for_each = each.value.tls != null ? [each.value.tls] : []
+    content {
+      validate_certificate_chain = tls.value.validate_certificate_chain
+      validate_certificate_name  = tls.value.validate_certificate_name
+    }
+  }
+}
